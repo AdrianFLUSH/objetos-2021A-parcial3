2533817f3a99b96163e163c46f54d4b2300122b6
Adrian <correo.d.adrian@gmail.com>
2021-05-17 09:23:28 -0500

    Examen

---
+++ src/uaslp/objetos/exams/Group.java
@@ -0,0 +1,43 @@
+package uaslp.objetos.exams;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Group {
+    private int Capacity;
+    private double GAverage;
+
+    List<Student> students;
+
+    public Group(int Capacity) {
+        this.Capacity = Capacity;
+    }
+
+    public int getCapacity() {
+        return Capacity;
+    }
+
+    public int getAvailability() {
+        return Capacity - students.size();
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) throws GroupIsFullException {
+        if (students.size() == Capacity) {
+            throw new GroupIsFullException();
+        } else
+            students.add(student);
+    }
+    public double getAverage() {
+
+        for(int i=0; i<students.size(); i++)
+        {
+                GAverage=GAverage+students.get(i).getAverage();
+        }
+        return GAverage/students.size();
+    }
+}
