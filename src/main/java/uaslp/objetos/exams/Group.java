4c468a479751433efea3f2ad08cddfda394a807f
Adrian <correo.d.adrian@gmail.com>
2021-05-19 11:27:11 -0500

    examen final

--- src/main/java/uaslp/objetos/exams/Group.java
+++ src/main/java/uaslp/objetos/exams/Group.java
@@ -1,17 +1,21 @@
 package uaslp.objetos.exams;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Group {
     private int Capacity;
+    private int availability;
     private double GAverage;
 
     List<Student> students;
 
     public Group(int Capacity) {
         this.Capacity = Capacity;
+        this.availability = Capacity;
+        students = new ArrayList<>(Capacity);
     }
 
     public int getCapacity() {
---
+++ src/main/java/uaslp/objetos/exams/Student.java
@@ -0,0 +1,43 @@
+package uaslp.objetos.exams;
+
+import java.util.List;
+
+public class Student {
+
+    private int code;
+    private String name;
+    private double average;
+    private Integer scores[];
+
+    public Student(String Name, int Code) {
+        this.name = Name;
+        this.code = Code;
+
+        scores = new Integer[3];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setScore(int index, int score) throws InvalidPartialException {
+        if (index < 1 || index > 3) {
+            throw new InvalidPartialException();
+        } else
+            scores[index - 1] = score;
+    }
+
+    public double getAverage() throws MissingScoreException {
+        for (int i = 0; i <3 ; i++) {
+            if (scores[i] == null) {
+                throw new MissingScoreException("Missing partial "+(i + 1));
+            }
+        }
+        average = (scores[0] + scores[1] + scores[2]) / 3;
+        return average;
+    }
+}
